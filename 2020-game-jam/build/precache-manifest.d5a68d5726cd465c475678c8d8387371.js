7937e58aa4639b7e4cf99f7e445684f7594de0d9
atticus cull <atticus@atticuss-mbp.lan>
2020-10-14 13:37:25 -0700

    B0 Initialize repository

---
+++ 2020-game-jam/build/precache-manifest.d5a68d5726cd465c475678c8d8387371.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc299035e675dd0e00479416cc1adce4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e52f93a072b85d82ab86",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "191269a4a53501933a84",
+    "url": "/static/js/2.26276a60.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.26276a60.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e52f93a072b85d82ab86",
+    "url": "/static/js/main.7d62acaa.chunk.js"
+  },
+  {
+    "revision": "65a0e077265a8c91df78",
+    "url": "/static/js/runtime-main.8fef0470.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
